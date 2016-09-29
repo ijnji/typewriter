5e5a25a0757826b55fb763fe182d4b31f8bf60a6
Ran Duan <rnduan@gmail.com>
2016-09-29 12:29:41 -0400

    Initial socket rooms for server and client

---
+++ browser/js/game/game.js
@@ -0,0 +1,36 @@
+app.config(function($stateProvider) {
+    $stateProvider.state('game', {
+        url: '/game/:gameId',
+        templateUrl: 'js/game/game.html',
+        controller: 'GameCtrl'
+    });
+});
+
+
+app.controller('GameCtrl', function($scope, $state, Socket) {
+
+    if ($state.params.gameId) {
+        Socket.emit('eventClientJoinGame', {
+            gameId: $state.params.gameId
+        });
+    }
+
+    Socket.on('eventServerRelayOne', function() {
+        let elem = document.getElementById('output');
+        elem.innerHTML += '<h3>Received eventServerRelayOne</h3>';
+    });
+
+    Socket.on('eventServerRelayTwo', function() {
+        let elem = document.getElementById('output');
+        elem.innerHTML += '<h3>Received eventServerRelayTwo</h3>';
+    });
+
+    $scope.sendOne = function() {
+        Socket.emit('eventClientOne');
+    };
+
+    $scope.sendTwo = function() {
+        Socket.emit('eventClientTwo');
+    };
+
+});

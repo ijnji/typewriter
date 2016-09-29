b3b106cb66569f448a1e55d730fdf94ef9cd86e7
Obiwancodi <gflemingiii@gmail.com>
2016-09-29 16:26:45 -0400

    fun

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,26 @@
+
+$(window).keydown(function(event) {
+	let fun = String.fromCharCode(event.which)
+	
+})
+
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         templateUrl: 'js/home/home.html'
     });
 });
+
+// app.controller('homeMecrtl', function($scope, $interval) {
+// 	$scope.position = "asf"
+// 	let number = 10
+// 	let init = $interval(function() {
+// 		number+=1
+// 		$scope.position = "-" + number + "px"
+		
+// 	}, 500)
+
+	
+// });
+
+

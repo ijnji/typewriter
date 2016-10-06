b03fcd52e01db10a7de15e000c75c0615a6ada5c
Sang Theresa Lee <tsahlee@gmail.com>
2016-10-06 17:25:25 -0400

    ok lobby works

---
+++ browser/js/common/factories/AccuracyFactory.js
@@ -0,0 +1,16 @@
+'use strict';
+
+app.factory('AccuracyFactory', function(Socket) {
+
+    let factory = factory;
+
+    return factory;
+
+    // let watchKeys = function() {
+    //     $(window).keydown(event => {
+    //         Socket.emit('keypress', {key: event.key, id: Socket.id});
+    //     })
+    // }
+
+
+});
--- server/io/EventHandlers/Lobby.js
+++ server/io/EventHandlers/Lobby.js
@@ -45,7 +45,7 @@ const loginUser = function (payload) {
     };
 
 const getUsers = function(){
-    this.socket.emit('users', {users: this.activeUsers});
+    this.io.emit('users', {users: this.activeUsers});
 }
 
 const challengeUser = function(payload){

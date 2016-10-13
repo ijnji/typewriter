ae230da26bee8a23cf2ca1d17c97d8fb256ac1bd
Babbins <alimahmo@buffalo.edu>
2016-10-13 12:50:50 -0400

    added matches model with basic definition

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,9 @@
 'use strict';
 var db = require('./_db');
 var User = require('./models/user');
+var Match = require('./models/match');
 module.exports = db;
 
+Match.belongsTo(User, {as: 'winner'});
+Match.belongsTo(User, {as: 'loser'});
 // eslint-disable-next-line no-unused-vars

539f13a7457b55cf029cfb8cd13f6e75c37045c1
Sang Theresa Lee <tsahlee@gmail.com>
2016-10-16 17:40:46 -0400

    ranking complete

--- browser/js/user/users.js
+++ browser/js/user/users.js
@@ -4,13 +4,38 @@ app.config(function($stateProvider) {
         templateUrl: 'js/user/users.html',
         controller: 'UsersCtrl',
         resolve: {
-            users: function(UserFactory){
+            users: function(UserFactory) {
                 return UserFactory.getAll();
             }
         }
     });
 });
 
-app.controller('UsersCtrl', function($scope, users){
+app.controller('UsersCtrl', function($scope, users) {
     $scope.users = users;
+
+    $scope.currentPage = 0;
+    $scope.pageSize = 10;
+    $scope.numberOfPages = function() {
+        return Math.ceil(users.length / $scope.pageSize);
+    }
 });
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
+
+// function MyCtrl($scope) {
+//     $scope.currentPage = 0;
+//     $scope.pageSize = 10;
+//     $scope.data = [];
+//     $scope.numberOfPages=function(){
+//         return Math.ceil($scope.data.length/$scope.pageSize);
+//     }
+//     for (var i=0; i<45; i++) {
+//         $scope.data.push("Item "+i);
+//     }
+// }
